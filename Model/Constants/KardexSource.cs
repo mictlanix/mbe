4ce5f9d2ebb80450a7dd998569361bf53be59955
Eduardo Nieto <enieto@e-kuiken.local>
2012-07-10 13:40:01 -0500

    kardex: work in progress...

---
+++ Model/Constants/KardexSource.cs
@@ -0,0 +1,51 @@
+// 
+// KardexSource.cs
+// 
+// Author:
+//   Eduardo Nieto <enieto@mictlanix.org>
+// 
+// Copyright (C) 2011 Eddy Zavaleta, Mictlanix, and contributors.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Castle.ActiveRecord;
+using Castle.ActiveRecord.Framework;
+
+namespace Mictlanix.BE.Model
+{
+    public enum KardexSource : int 
+    {
+        [Display(Name = "Sale", ResourceType = typeof(Resources))]
+        Sale = 1,
+        [Display(Name = "Purchase", ResourceType = typeof(Resources))]
+        Purchase, 
+		[Display(Name = "InventoryIssue", ResourceType = typeof(Resources))]
+        InventoryIssue,
+		[Display(Name = "InventoryReceipt", ResourceType = typeof(Resources))]
+        InventoryReceipt,
+		[Display(Name = "InventoryTransfer", ResourceType = typeof(Resources))]
+        InventoryTransfer
+    }
+}
+
