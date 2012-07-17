b32abe0179440ccf463697d4681d3122cdce9bee
Eduardo Nieto <enieto@mictlanix.org>
2012-07-16 20:26:53 -0500

    working in kardex

---
+++ Web/Models/ItemDateRange.cs
@@ -0,0 +1,56 @@
+﻿// 
+// DateRange.cs
+// 
+// Author:
+//   Eddy Zavaleta <eddy@mictlanix.org>
+//   Eduardo Nieto <enieto@mictlanix.org>
+//
+// Copyright (C) 2011 Eddy Zavaleta, Mictlanix, and contributors.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using Mictlanix.BE.Model;
+
+namespace Mictlanix.BE.Web.Models
+{
+	public class ItemDateRange<T> : DateRange {
+	    public ItemDateRange()
+        {
+	    }
+
+        public ItemDateRange(T item, DateTime start, DateTime end)
+        {
+	        this.Item = item;
+            this.StartDate = start;
+            this.EndDate = end;
+	    }
+
+        public T Item { get; set; }
+        public int ItemId { get; set; }
+        
+	}
+}
