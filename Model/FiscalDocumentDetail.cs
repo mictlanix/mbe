07621d9450ec87cbd668a086aeaacf754f650cd3
Eddy Zavaleta <eddy@mictlanix.org>
2013-07-10 19:31:29 -0500

    mictlanix domain updated

--- Model/FiscalDocumentDetail.cs
+++ Model/FiscalDocumentDetail.cs
@@ -2,7 +2,7 @@
 // FiscalDocumentDetail.cs
 // 
 // Author:
-//   Eddy Zavaleta <eddy@mictlanix.org>
+//   Eddy Zavaleta <eddy@mictlanix.com>
 // 
 // Copyright (C) 2013 Eddy Zavaleta, Mictlanix, and contributors.
 // 
--- Web/Controllers/DeliveryOrdersController.cs
+++ Web/Controllers/DeliveryOrdersController.cs
@@ -2,7 +2,7 @@
 // DeliveryOrdersController.cs
 // 
 // Author:
-//   Eddy Zavaleta <eddy@mictlanix.org>
+//   Eddy Zavaleta <eddy@mictlanix.com>
 // 
 // Copyright (C) 2013 Eddy Zavaleta, Mictlanix, and contributors.
 // 
