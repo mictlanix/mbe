728ac89bd1e840b0cf47a7a73da448e871ddaa33
Eddy Zavaleta <eddy@mictlanix.org>
2013-10-24 05:05:28 -0500

    13.10 release

--- Schema/changes/mbe-lastest.sql
+++
@@ -1,9 +0,0 @@
-
-UPDATE taxpayer_batch  SET type = 100 WHERE type = 1;
-UPDATE fiscal_document SET type = 100 WHERE type = 1;
-
-ALTER TABLE `fiscal_document_detail` 
-  ADD COLUMN `comment` VARCHAR(500) NULL;
-
-ALTER TABLE `taxpayer` 
-  CHANGE COLUMN `name` `name` VARCHAR(250) NULL;
