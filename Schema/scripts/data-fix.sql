8d6c15ff112059fd9b4e643c896acc26b0d2245c
Eddy Zavaleta <eddy@mictlanix.com>
2014-08-11 15:58:08 -0500

    bugfix: customer's selector doesn't search by code

--- Schema/scripts/data-fix.sql
+++ Schema/scripts/data-fix.sql
@@ -26,3 +26,13 @@ WHERE unit_of_measurement IN ('MTS');
 SELECT DISTINCT unit_of_measurement
 FROM product
 ORDER BY 1;
+
+SELECT fiscal_document_xml_id
+FROM fiscal_document_xml x
+INNER JOIN fiscal_document f ON x.fiscal_document_xml_id = f.fiscal_document_id
+WHERE version = 3.2 AND LOCATE('tfd:TimbreFiscalDigital version', data) <> 0
+
+UPDATE fiscal_document_xml x  
+INNER JOIN fiscal_document f  ON x.fiscal_document_xml_id = f.fiscal_document_id 
+SET  x.data = REPLACE(x.data, 'tfd:TimbreFiscalDigital version', 'tfd:TimbreFiscalDigital xsi:schemaLocation="http://www.sat.gob.mx/TimbreFiscalDigital http://www.sat.gob.mx/sitio_internet/TimbreFiscalDigital/TimbreFiscalDigital.xsd" version') 
+WHERE f.version = 3.2 AND LOCATE('tfd:TimbreFiscalDigital version', x.data) <> 0
