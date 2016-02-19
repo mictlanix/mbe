5d23b6e8993419a98ab463109f2abed558aab831
Eddy Zavaleta <eddy@mictlanix.com>
2016-02-19 03:28:57 -0600

    unused properties removed

--- Model/TaxpayerBatch.cs
+++ Model/TaxpayerBatch.cs
@@ -25,25 +25,7 @@ namespace Mictlanix.BE.Model
 		[Property]
 		[Display(Name = "Batch", ResourceType = typeof(Resources))]
 		public virtual string Batch { get; set; }
-		
-		[Property("serial_start")]
-        [Display(Name = "SerialStart", ResourceType = typeof(Resources))]
-		public virtual int SerialStart { get; set; }
-		
-		[Property("serial_end")]
-        [Display(Name = "SerialEnd", ResourceType = typeof(Resources))]
-		public virtual int SerialEnd { get; set; }
-		
-		[Property("approval_number")]
-        [Required(ErrorMessageResourceName = "Validation_Required", ErrorMessageResourceType = typeof(Resources))]
-        [Display(Name = "ApprovalNumber", ResourceType = typeof(Resources))]
-		public virtual int? ApprovalNumber { get; set; }
-		
-		[Property("approval_year")]
-		[Required(ErrorMessageResourceName = "Validation_Required", ErrorMessageResourceType = typeof(Resources))]
-		[Display(Name = "ApprovalYear", ResourceType = typeof(Resources))]
-		public virtual int? ApprovalYear { get; set; }
-		
+
 		[Property]
         [Display(Name = "Type", ResourceType = typeof(Resources))]
 		public virtual FiscalDocumentType Type { get; set; }
@@ -56,7 +38,7 @@ namespace Mictlanix.BE.Model
 
 		public override string ToString ()
 		{
-			return string.Format ("{0} [{1:D5} - {2:D5}])", Batch, SerialStart, SerialEnd);
+			return string.Format ("{0})", Batch);
 		}
 
 		public override bool Equals (object obj)
