823d60518585884cba59fe69f266e937a112a2b3
Eddy Zavaleta <eddy@mictlanix.com>
2016-03-15 15:28:43 -0600

    bugfix: duplicate entry for store & serial keys

--- Model/InventoryIssue.cs
+++ Model/InventoryIssue.cs
@@ -34,107 +34,104 @@ using Castle.ActiveRecord;
 using Castle.ActiveRecord.Framework;
 using System.ComponentModel.DataAnnotations;
 
-namespace Mictlanix.BE.Model
-{
-    [ActiveRecord("inventory_issue")]
-    public class InventoryIssue : ActiveRecordLinqBase<InventoryIssue>
-    {
-        IList<InventoryIssueDetail> details = new List<InventoryIssueDetail>();
+namespace Mictlanix.BE.Model {
+	[ActiveRecord ("inventory_issue")]
+	public class InventoryIssue : ActiveRecordLinqBase<InventoryIssue> {
+		IList<InventoryIssueDetail> details = new List<InventoryIssueDetail> ();
 
-        [PrimaryKey(PrimaryKeyType.Identity, "inventory_issue_id")]
-        [Display(Name = "InventoryIssueId", ResourceType = typeof(Resources))]
-		[DisplayFormat(DataFormatString = "{0:D8}")]
+		[PrimaryKey (PrimaryKeyType.Identity, "inventory_issue_id")]
+		[Display (Name = "InventoryIssueId", ResourceType = typeof (Resources))]
+		[DisplayFormat (DataFormatString = "{0:D8}")]
 		public int Id { get; set; }
 
-		[BelongsTo("store")]
-		[Display(Name = "Store", ResourceType = typeof(Resources))]
+		[BelongsTo ("store")]
+		[Display (Name = "Store", ResourceType = typeof (Resources))]
 		public virtual Store Store { get; set; }
 
-		[Property("serial")]
-		[Display(Name = "Serial", ResourceType = typeof(Resources))]
-		[DisplayFormat(DataFormatString="{0:D8}")]
-		public virtual int Serial { get; set; }
+		[Property ("serial")]
+		[Display (Name = "Serial", ResourceType = typeof (Resources))]
+		[DisplayFormat (DataFormatString = "{0:D8}")]
+		public virtual int? Serial { get; set; }
 
-        [Property("creation_time")]
-        [DataType(DataType.DateTime)]
-        [Display(Name = "CreationTime", ResourceType = typeof(Resources))]
-        public DateTime CreationTime { get; set; }
+		[Property ("creation_time")]
+		[DataType (DataType.DateTime)]
+		[Display (Name = "CreationTime", ResourceType = typeof (Resources))]
+		public DateTime CreationTime { get; set; }
 
-        [Property("modification_time")]
-        [DataType(DataType.DateTime)]
-        [Display(Name = "ModificationTime", ResourceType = typeof(Resources))]
-        public DateTime ModificationTime { get; set; }
+		[Property ("modification_time")]
+		[DataType (DataType.DateTime)]
+		[Display (Name = "ModificationTime", ResourceType = typeof (Resources))]
+		public DateTime ModificationTime { get; set; }
 
-        [BelongsTo("creator")]
-        [Display(Name = "Creator", ResourceType = typeof(Resources))]
-        public virtual Employee Creator { get; set; }
+		[BelongsTo ("creator")]
+		[Display (Name = "Creator", ResourceType = typeof (Resources))]
+		public virtual Employee Creator { get; set; }
 
-        [BelongsTo("updater")]
-        [Display(Name = "Updater", ResourceType = typeof(Resources))]
-        public virtual Employee Updater { get; set; }
+		[BelongsTo ("updater")]
+		[Display (Name = "Updater", ResourceType = typeof (Resources))]
+		public virtual Employee Updater { get; set; }
 
-        [Required(ErrorMessageResourceName = "Validation_Required", ErrorMessageResourceType = typeof(Resources))]
-        [Display(Name = "Warehouse", ResourceType = typeof(Resources))]
-        [UIHint("WarehouseSelector")]
-        public int WarehouseId { get; set; }
+		[Required (ErrorMessageResourceName = "Validation_Required", ErrorMessageResourceType = typeof (Resources))]
+		[Display (Name = "Warehouse", ResourceType = typeof (Resources))]
+		[UIHint ("WarehouseSelector")]
+		public int WarehouseId { get; set; }
 
-        [BelongsTo("warehouse")]
-        [Display(Name = "Warehouse", ResourceType = typeof(Resources))]
-        public virtual Warehouse Warehouse { get; set; }
+		[BelongsTo ("warehouse")]
+		[Display (Name = "Warehouse", ResourceType = typeof (Resources))]
+		public virtual Warehouse Warehouse { get; set; }
 
-        [BelongsTo("supplier_return")]
-        [Display(Name = "SupplierReturn", ResourceType = typeof(Resources))]
-        public virtual SupplierReturn Return { get; set; }
+		[BelongsTo ("supplier_return")]
+		[Display (Name = "SupplierReturn", ResourceType = typeof (Resources))]
+		public virtual SupplierReturn Return { get; set; }
 
-        [Property("completed")]
-        [Display(Name = "Completed", ResourceType = typeof(Resources))]
-        public bool IsCompleted { get; set; }
+		[Property ("completed")]
+		[Display (Name = "Completed", ResourceType = typeof (Resources))]
+		public bool IsCompleted { get; set; }
 
-        [Property("cancelled")]
-        [Display(Name = "Cancelled", ResourceType = typeof(Resources))]
-        public bool IsCancelled { get; set; }
+		[Property ("cancelled")]
+		[Display (Name = "Cancelled", ResourceType = typeof (Resources))]
+		public bool IsCancelled { get; set; }
 
-        [Property]
-        [DataType(DataType.MultilineText)]
-        [Display(Name = "Comment", ResourceType = typeof(Resources))]
-		[StringLength(500, MinimumLength = 0, ErrorMessageResourceName = "Validation_StringLength", ErrorMessageResourceType = typeof(Resources))]
-        public string Comment { get; set; }
+		[Property]
+		[DataType (DataType.MultilineText)]
+		[Display (Name = "Comment", ResourceType = typeof (Resources))]
+		[StringLength (500, MinimumLength = 0, ErrorMessageResourceName = "Validation_StringLength", ErrorMessageResourceType = typeof (Resources))]
+		public string Comment { get; set; }
 
-        [HasMany(typeof(InventoryIssueDetail), Table = "inventory_issue_detail", ColumnKey = "issue")]
-        public IList<InventoryIssueDetail> Details
-        {
-            get { return details; }
-            set { details = value; }
-        }
+		[HasMany (typeof (InventoryIssueDetail), Table = "inventory_issue_detail", ColumnKey = "issue")]
+		public IList<InventoryIssueDetail> Details {
+			get { return details; }
+			set { details = value; }
+		}
 
-        #region Override Base Methods
+		#region Override Base Methods
 
-        public override string ToString()
-        {
-            return string.Format("{0} [{1}, {2}, {3}]", Id, CreationTime, Creator, Warehouse);
-        }
+		public override string ToString ()
+		{
+			return string.Format ("{0} [{1}, {2}, {3}]", Id, CreationTime, Creator, Warehouse);
+		}
 
-        public override bool Equals(object obj)
-        {
-            InventoryIssue other = obj as InventoryIssue;
+		public override bool Equals (object obj)
+		{
+			InventoryIssue other = obj as InventoryIssue;
 
-            if (other == null)
-                return false;
+			if (other == null)
+				return false;
 
-            if (Id == 0 && other.Id == 0)
-                return (object)this == other;
-            else
-                return Id == other.Id;
-        }
+			if (Id == 0 && other.Id == 0)
+				return (object) this == other;
+			else
+				return Id == other.Id;
+		}
 
-        public override int GetHashCode()
-        {
-            if (Id == 0)
-                return base.GetHashCode();
+		public override int GetHashCode ()
+		{
+			if (Id == 0)
+				return base.GetHashCode ();
 
-            return string.Format("{0}#{1}", GetType().FullName, Id).GetHashCode();
-        }
+			return string.Format ("{0}#{1}", GetType ().FullName, Id).GetHashCode ();
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- Model/InventoryReceipt.cs
+++ Model/InventoryReceipt.cs
@@ -34,107 +34,104 @@ using Castle.ActiveRecord;
 using Castle.ActiveRecord.Framework;
 using System.ComponentModel.DataAnnotations;
 
-namespace Mictlanix.BE.Model
-{
-    [ActiveRecord("inventory_receipt")]
-    public class InventoryReceipt : ActiveRecordLinqBase<InventoryReceipt>
-    {
-        IList<InventoryReceiptDetail> details = new List<InventoryReceiptDetail>();
+namespace Mictlanix.BE.Model {
+	[ActiveRecord ("inventory_receipt")]
+	public class InventoryReceipt : ActiveRecordLinqBase<InventoryReceipt> {
+		IList<InventoryReceiptDetail> details = new List<InventoryReceiptDetail> ();
 
-        [PrimaryKey(PrimaryKeyType.Identity, "inventory_receipt_id")]
-        [Display(Name = "InventoryReceiptId", ResourceType = typeof(Resources))]
-		[DisplayFormat(DataFormatString = "{0:D8}")]
+		[PrimaryKey (PrimaryKeyType.Identity, "inventory_receipt_id")]
+		[Display (Name = "InventoryReceiptId", ResourceType = typeof (Resources))]
+		[DisplayFormat (DataFormatString = "{0:D8}")]
 		public int Id { get; set; }
 
-		[BelongsTo("store")]
-		[Display(Name = "Store", ResourceType = typeof(Resources))]
+		[BelongsTo ("store")]
+		[Display (Name = "Store", ResourceType = typeof (Resources))]
 		public virtual Store Store { get; set; }
 
-		[Property("serial")]
-		[Display(Name = "Serial", ResourceType = typeof(Resources))]
-		[DisplayFormat(DataFormatString="{0:D8}")]
-		public virtual int Serial { get; set; }
+		[Property ("serial")]
+		[Display (Name = "Serial", ResourceType = typeof (Resources))]
+		[DisplayFormat (DataFormatString = "{0:D8}")]
+		public virtual int? Serial { get; set; }
 
-        [Property("creation_time")]
-        [DataType(DataType.DateTime)]
-        [Display(Name = "CreationTime", ResourceType = typeof(Resources))]
-        public DateTime CreationTime { get; set; }
+		[Property ("creation_time")]
+		[DataType (DataType.DateTime)]
+		[Display (Name = "CreationTime", ResourceType = typeof (Resources))]
+		public DateTime CreationTime { get; set; }
 
-        [Property("modification_time")]
-        [DataType(DataType.DateTime)]
-        [Display(Name = "ModificationTime", ResourceType = typeof(Resources))]
-        public DateTime ModificationTime { get; set; }
+		[Property ("modification_time")]
+		[DataType (DataType.DateTime)]
+		[Display (Name = "ModificationTime", ResourceType = typeof (Resources))]
+		public DateTime ModificationTime { get; set; }
 
-        [BelongsTo("creator")]
-        [Display(Name = "Creator", ResourceType = typeof(Resources))]
-        public virtual Employee Creator { get; set; }
+		[BelongsTo ("creator")]
+		[Display (Name = "Creator", ResourceType = typeof (Resources))]
+		public virtual Employee Creator { get; set; }
 
-        [BelongsTo("updater")]
-        [Display(Name = "Updater", ResourceType = typeof(Resources))]
-        public virtual Employee Updater { get; set; }
+		[BelongsTo ("updater")]
+		[Display (Name = "Updater", ResourceType = typeof (Resources))]
+		public virtual Employee Updater { get; set; }
 
-        [Required(ErrorMessageResourceName = "Validation_Required", ErrorMessageResourceType = typeof(Resources))]
-        [Display(Name = "Warehouse", ResourceType = typeof(Resources))]
-        [UIHint("WarehouseSelector")]
-        public int WarehouseId { get; set; }
+		[Required (ErrorMessageResourceName = "Validation_Required", ErrorMessageResourceType = typeof (Resources))]
+		[Display (Name = "Warehouse", ResourceType = typeof (Resources))]
+		[UIHint ("WarehouseSelector")]
+		public int WarehouseId { get; set; }
 
-        [BelongsTo("warehouse")]
-        [Display(Name = "Warehouse", ResourceType = typeof(Resources))]
-        public virtual Warehouse Warehouse { get; set; }
+		[BelongsTo ("warehouse")]
+		[Display (Name = "Warehouse", ResourceType = typeof (Resources))]
+		public virtual Warehouse Warehouse { get; set; }
 
-        [BelongsTo("purchase_order")]
-        [Display(Name = "PurchaseOrder", ResourceType = typeof(Resources))]
-        public virtual PurchaseOrder Order { get; set; }
+		[BelongsTo ("purchase_order")]
+		[Display (Name = "PurchaseOrder", ResourceType = typeof (Resources))]
+		public virtual PurchaseOrder Order { get; set; }
 
-        [Property("completed")]
-        [Display(Name = "Completed", ResourceType = typeof(Resources))]
-        public bool IsCompleted { get; set; }
+		[Property ("completed")]
+		[Display (Name = "Completed", ResourceType = typeof (Resources))]
+		public bool IsCompleted { get; set; }
 
-        [Property("cancelled")]
-        [Display(Name = "Cancelled", ResourceType = typeof(Resources))]
-        public bool IsCancelled { get; set; }
+		[Property ("cancelled")]
+		[Display (Name = "Cancelled", ResourceType = typeof (Resources))]
+		public bool IsCancelled { get; set; }
 
-        [Property]
-        [DataType(DataType.MultilineText)]
-        [Display(Name = "Comment", ResourceType = typeof(Resources))]
-		[StringLength(500, MinimumLength = 0, ErrorMessageResourceName = "Validation_StringLength", ErrorMessageResourceType = typeof(Resources))]
-        public string Comment { get; set; }
+		[Property]
+		[DataType (DataType.MultilineText)]
+		[Display (Name = "Comment", ResourceType = typeof (Resources))]
+		[StringLength (500, MinimumLength = 0, ErrorMessageResourceName = "Validation_StringLength", ErrorMessageResourceType = typeof (Resources))]
+		public string Comment { get; set; }
 
-        [HasMany(typeof(InventoryReceiptDetail), Table = "inventory_receipt_detail", ColumnKey = "receipt")]
-        public IList<InventoryReceiptDetail> Details
-        {
-            get { return details; }
-            set { details = value; }
-        }
+		[HasMany (typeof (InventoryReceiptDetail), Table = "inventory_receipt_detail", ColumnKey = "receipt")]
+		public IList<InventoryReceiptDetail> Details {
+			get { return details; }
+			set { details = value; }
+		}
 
-        #region Override Base Methods
+		#region Override Base Methods
 
-        public override string ToString()
-        {
-            return string.Format("{0} [{1}, {2}, {3}]", Id, CreationTime, Creator, Warehouse);
-        }
+		public override string ToString ()
+		{
+			return string.Format ("{0} [{1}, {2}, {3}]", Id, CreationTime, Creator, Warehouse);
+		}
 
-        public override bool Equals(object obj)
-        {
-            InventoryReceipt other = obj as InventoryReceipt;
+		public override bool Equals (object obj)
+		{
+			InventoryReceipt other = obj as InventoryReceipt;
 
-            if (other == null)
-                return false;
+			if (other == null)
+				return false;
 
-            if (Id == 0 && other.Id == 0)
-                return (object)this == other;
-            else
-                return Id == other.Id;
-        }
+			if (Id == 0 && other.Id == 0)
+				return (object) this == other;
+			else
+				return Id == other.Id;
+		}
 
-        public override int GetHashCode()
-        {
-            if (Id == 0)
-                return base.GetHashCode();
+		public override int GetHashCode ()
+		{
+			if (Id == 0)
+				return base.GetHashCode ();
 
-            return string.Format("{0}#{1}", GetType().FullName, Id).GetHashCode();
-        }
+			return string.Format ("{0}#{1}", GetType ().FullName, Id).GetHashCode ();
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
