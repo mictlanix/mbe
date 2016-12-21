f853dea5b7b91b1dc59ee43db0eec96219a1b03f
Eddy Zavaleta <eddy@mictlanix.com>
2016-12-21 09:09:08 -0600

    payment allocations added

---
+++ Web/Models/AccountReceivable.cs
@@ -0,0 +1,63 @@
+﻿// 
+// AccountsReceivableSummary.cs
+// 
+// Author:
+//   Eddy Zavaleta <eddy@mictlanix.com>
+// 
+// Copyright (C) 2016 Eddy Zavaleta, Mictlanix, and contributors.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web.Security;
+using Mictlanix.BE.Model;
+
+namespace Mictlanix.BE.Web.Models {
+	public class AccountReceivable {
+		[Display (Name = "Customer", ResourceType = typeof (Resources))]
+		public string Customer { get; set; }
+
+		[DataType (DataType.Currency)]
+		[Display (Name = "PaymentsSummary", ResourceType = typeof (Resources))]
+		public decimal PaymentsSummary { get; set; }
+
+		[DataType (DataType.Currency)]
+		[Display (Name = "SalesSummary", ResourceType = typeof (Resources))]
+		public decimal SalesSummary { get; set; }
+
+		[DataType (DataType.Currency)]
+		[Display (Name = "ReturnsSummary", ResourceType = typeof (Resources))]
+		public decimal ReturnsSummary { get; set; }
+
+		[DataType (DataType.Currency)]
+		[Display (Name = "Balance", ResourceType = typeof (Resources))]
+		public decimal Balance {
+			get {
+				return SalesSummary - PaymentsSummary - ReturnsSummary;
+			}
+		}
+	}
+
+}
