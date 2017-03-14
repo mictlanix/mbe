e9ab2ad9c113f24c06fcc6071c574f233c1d932a
Eddy Zavaleta <eddy@mictlanix.com>
2017-03-14 16:21:03 -0600

    db scripts updated

--- Schema/scripts/data-default.sql
+++ Schema/scripts/data-default.sql
@@ -1,8 +1,12 @@
-DELETE FROM price_list;
+-- DELETE FROM customer;
+-- DELETE FROM price_list;
 
 INSERT INTO price_list (`price_list_id`,`name`, `high_profit_margin`, `low_profit_margin`)
-VALUES (1,'Mostrador',0,0),(2,'Super Mostrador',0,0),(3,'Mayoreo',0,0),(4,'Especial',0,0);
+VALUES (1,'General',0,0),(999999,'Costo',0,0);
 
-INSERT INTO customer (customer_id, name, credit_limit, credit_days, price_list)
-VALUES (1,'Público en General',0,0,1);
+UPDATE price_list SET price_list_id = 0 WHERE price_list_id = 999999;
 
+ALTER TABLE price_list AUTO_INCREMENT=2;
+
+INSERT INTO customer (customer_id, code, name, credit_limit, credit_days, price_list)
+VALUES (1,'PG','Público en General',0,0,1);
